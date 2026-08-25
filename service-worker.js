7f9453c3d0df92c4d17099b95dd97f1e704ca166
brentlyj-creator <brentlyj@gmail.com>
2026-08-25 11:44:45 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nova-recipe-builder-github-pages-v100';
+const CACHE_NAME = 'nova-recipe-builder-github-pages-v101';
 const BASE_PATH = '/nova-recipe-builder/';
 
 const APP_SHELL = [
